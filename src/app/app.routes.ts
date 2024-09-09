877499bd4b88511e4c6fa599e3749cadcf4aa9a2
Miko-29 <muskan2912002@gmail.com>
2024-09-09 17:31:57 +0530

    polygon

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { MarkerComponent } from './marker/marker.component';
 import { AirportsComponent } from './airports/airports.component';
 import { PolylineComponent } from './polyline/polyline.component';
+import { PolygonComponent } from './polygon/polygon.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'marker',pathMatch:'full'},
     {path:'marker',component:MarkerComponent},
     {path:'airports',component:AirportsComponent},
     {path:'polyline',component:PolylineComponent},
+    {path:'polygon',component:PolygonComponent},
 ];
