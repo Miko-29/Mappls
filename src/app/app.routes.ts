33c6ee7c8c191a82fcbe5b129f8936ba5d5193ef
Miko-29 <muskan2912002@gmail.com>
2024-09-09 10:44:59 +0530

    polyline

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { MarkerComponent } from './marker/marker.component';
 import { AirportsComponent } from './airports/airports.component';
+import { PolylineComponent } from './polyline/polyline.component';
 
 export const routes: Routes = [
     {path:'',redirectTo:'marker',pathMatch:'full'},
     {path:'marker',component:MarkerComponent},
-    {path:'airports',component:AirportsComponent}
+    {path:'airports',component:AirportsComponent},
+    {path:'polyline',component:PolylineComponent},
 ];
---
+++ src/app/polyline/polyline.component.ts
@@ -0,0 +1,84 @@
+import { Component } from '@angular/core';
+import { mappls, mappls_plugin } from 'mappls-web-maps';
+
+@Component({
+  selector: 'app-polyline',
+  standalone: true,
+  imports: [],
+  templateUrl: './polyline.component.html',
+  styleUrl: './polyline.component.css'
+})
+export class PolylineComponent {
+  polylineData = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "LineString",
+          "coordinates": [
+            [85.2000, 23.3441],
+            [85.323597, 23.317725]
+          ]
+        },
+        "properties": {
+          "name": "Ranchi Airport to Ranchi High Court"
+        }
+      }
+    ]
+  };
+  mapObject: any;
+  mapplsClassObject: any = new mappls()
+  mapplsPluginObject: any = new mappls_plugin()
+  polylineObject: any;
+  markerObject1: any;
+  markerObject2: any;
+
+  ngOnInit() {
+    const loadObject = {
+      map: true,
+      layer: 'vector',
+      libraries: ['airspacelayers'],
+      // plugins: ['direction'],
+
+    };
+
+    this.mapplsClassObject.initialize(
+      '3977bfeea17933aaecdd1c90c171b587', loadObject,
+      () => {
+        this.mapObject = this.mapplsClassObject.Map(
+          {
+            id: 'map',
+            properties: {
+              center: [23.3441, 85.3096],
+              zoomControl: true,
+              location: true,
+              // zoom:
+            },
+          },
+          // (data:any)=>{ this.mapObject = data ;  }
+
+        )
+        // console.log('map');
+        this.polylineObject = this.mapplsClassObject.Polyline(
+          {
+            map: this.mapObject,
+            path: this.polylineData,
+            strokeColor:  'green',
+          }
+        );
+
+        this.markerObject1 = this.mapplsClassObject.Marker({
+          map: this.mapObject,
+          position: { lat: 23.317725, lng: 85.323597 },
+          popupHtml: 'Birsa Munda International Airport',
+        });
+
+        this.markerObject1 = this.mapplsClassObject.Marker({
+          map: this.mapObject,
+          position: { lat: 23.3441, lng: 85.2000 },
+          popupHtml: 'Jharkhand High Court',
+        });
+      })
+  }
+}
